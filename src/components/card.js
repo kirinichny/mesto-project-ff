8c9480c15c850c3c17539e4b456c7cdda92c5a74
Kirin <kirin-89@yandex.ru>
2024-04-11 04:30:08 +1000

    feat: Implement modal functionality for profile edit, card addition, photo preview, card liking

--- src/components/card.js
+++ src/components/card.js
@@ -1,4 +1,15 @@
 /**
+ * Константы для определения позиции вставки карты.
+ * @type {Object}
+ * @property {string} END - Карта должна быть вставлен в конец контейнера.
+ * @property {string} START - Карта должна быть вставлен в начало контейнера.
+ */
+const INSERT_POSITION = {
+    END: 'end',
+    START: 'start'
+};
+
+/**
  * Создает карточку на основе шаблона.
  *
  * @param {Object} cardParams Объект с параметрами для создания карточки.
@@ -6,51 +17,71 @@
  * @param {string} cardParams.cardData.name Название карточки.
  * @param {string} cardParams.cardData.link Ссылка на изображение карточки.
  * @param {Element} cardParams.cardTemplate Шаблон карточки.
- * @param {Object} cardParams.cardSelectors Селекторы карточки.
- * @param {string} cardParams.cardSelectors.image Селектор изображения карточки.
- * @param {string} cardParams.cardSelectors.title Селектор заголовка карточки.
- * @param {string} cardParams.cardSelectors.deleteButton Селектор кнопки удаления карточки.
- * @param {string} cardParams.cardSelectors.likeButton Селектор кнопки лайка.
- * @param {string} cardParams.cardSelectors.likeButtonIsActive Класс проставленного лайка.
- * @param {Function} [cardParams.onDelete=deleteCard] Функция, вызываемая при удалении карточки.
- * @param {Function} [cardParams.onLike=toggleLikeState] Функция, вызываемая при нажатии на лайк.
+ * @param {Object} cardParams.cardClasses Классы карточки.
+ * @param {string} cardParams.cardClasses.image Класс изображения карточки.
+ * @param {string} cardParams.cardClasses.title Класс заголовка карточки.
+ * @param {string} cardParams.cardClasses.deleteButton Класс кнопки удаления карточки.
+ * @param {string} cardParams.cardClasses.likeButton Класс кнопки лайка.
+ * @param {string} cardParams.cardClasses.likeButtonIsActive Класс проставленного лайка.
+ * @param {Function} cardParams.onClick Функция, вызываемая при нажатии на карточку.
+ * @param {Function} [cardParams.onDelete=deleteCard] Функция, вызываемая при удалении карточки. По умолчанию "deleteCard".
+ * @param {Function} [cardParams.onLike=toggleLikeState] Функция, вызываемая при нажатии на лайк. По умолчанию "toggleLikeState".
  * @return {HTMLElement} Элемент карточки.
  */
-function createCard({cardData, cardTemplate, cardSelectors, onDelete = deleteCard, onLike = toggleLikeState}) {
+function createCard({cardData, cardTemplate, cardClasses, onClick, onDelete = deleteCard, onLike = toggleLikeState}) {
     const cardElement = cardTemplate.cloneNode(true);
-    const cardImage = cardElement.querySelector(cardSelectors.image);
-    const cardTitle = cardElement.querySelector(cardSelectors.title);
-    const deleteButton = cardElement.querySelector(cardSelectors.deleteButton);
-    const likeButton = cardElement.querySelector(cardSelectors.likeButton);
+    const cardImage = cardElement.querySelector(`.${cardClasses.image}`);
+    const cardTitle = cardElement.querySelector(`.${cardClasses.title}`);
+    const deleteButton = cardElement.querySelector(`.${cardClasses.deleteButton}`);
+    const likeButton = cardElement.querySelector(`.${cardClasses.likeButton}`);
 
     cardImage.src = cardData.link;
     cardImage.alt = cardData.name;
     cardTitle.textContent = cardData.name;
 
+    cardImage.addEventListener('click', () => {
+        onClick(cardData)
+    });
     deleteButton.addEventListener('click', () => onDelete(cardElement));
-    likeButton.addEventListener('click', (evt) => toggleLikeState(evt, cardSelectors.likeButtonIsActive));
+    likeButton.addEventListener('click', (evt) => toggleLikeState(evt, cardClasses.likeButtonIsActive));
 
     return cardElement;
 }
 
 /**
+ * Вставляет карточку в указанный элемент.
+ *
+ * @param {Element} targetElement - Элемент DOM, в который будет вставлена карточка.
+ * @param {Element} cardElement - Элемент карточки, который необходимо вставить.
+ * @param {string} [insertPosition='end'] - Позиция вставки карточки.
+ *                                          Допустимые значения: 'start', 'end'.
+ *                                          По умолчанию 'end'.
+ */
+function insertCard(targetElement, cardElement, insertPosition = 'end') {
+    if (insertPosition === 'start') {
+        targetElement.prepend(cardElement);
+    } else {
+        targetElement.append(cardElement);
+    }
+}
+
+/**
  * Удаляет указанную карточку.
  *
- * @param {Element} cardElement - Элемент карточки, который нужно удалить.
+ * @param {Element} cardElement - Элемент карточки, который необходимо удалить.
  */
 function deleteCard(cardElement) {
     cardElement.remove();
 }
 
-
 /**
  * Переключает состояние "лайка".
  *
  * @param {Event} evt - объект события, который был инициирован.
- * @param {string} selectorLikeIsActive Класс проставленного лайка.
+ * @param {string} likeIsActiveClass Класс проставленного лайка.
  */
-function toggleLikeState(evt, selectorLikeIsActive) {
-    evt.target.classList.toggle(selectorLikeIsActive);
+function toggleLikeState(evt, likeIsActiveClass) {
+    evt.target.classList.toggle(likeIsActiveClass);
 }
 
-export {createCard, deleteCard};
+export {INSERT_POSITION, createCard, insertCard, deleteCard};
--- src/components/index.js
+++ src/components/index.js
@@ -1,22 +1,111 @@
 import '../pages/index.css';
 import {initialCards} from "./card-data";
-import {createCard} from "./card";
+import {INSERT_POSITION, createCard, insertCard} from "./card";
+import {openModal, closeModal, setImageModalData} from "./modal";
 
 const placesList = document.querySelector('.places__list');
 const cardTemplate = document.querySelector('#card-template').content.children[0];
-const cardSelectors = {
-    image: ".card__image",
-    title: ".card__title",
-    deleteButton: ".card__delete-button",
-    likeButton: ".card__like-button",
+
+const profileTitle = document.querySelector('.profile__title');
+const profileDescription = document.querySelector('.profile__description');
+
+const editProfileButton = document.querySelector('.profile__edit-button');
+const addButton = document.querySelector('.profile__add-button');
+
+const editProfileModal = document.querySelector('.popup_type_edit');
+const newCardModal = document.querySelector('.popup_type_new-card');
+const imageModal = document.querySelector('.popup_type_image');
+const allModalWindows = document.querySelectorAll('.popup');
+
+const editProfileForm = document.forms['edit-profile'];
+const newCardForm = document.forms['new-place'];
+
+const modalClasses = {
+    isOpened: "popup_is-opened",
+    closeButton: "popup__close"
+};
+const imageModalClasses = {
+    image: 'popup__image',
+    caption: 'popup__caption'
+};
+const cardClasses = {
+    image: "card__image",
+    title: "card__title",
+    deleteButton: "card__delete-button",
+    likeButton: "card__like-button",
     likeButtonIsActive: "card__like-button_is-active"
 }
 
-function renderCards(cards) {
+function renderImageModal(imageData) {
+    setImageModalData(imageModal, imageData, imageModalClasses)
+    openModal(imageModal, modalClasses.isOpened);
+}
+
+function renderCard(cardData, insertPosition) {
+    const cardParams = {cardData, cardTemplate, cardClasses, onClick: renderImageModal}
+    const cardElement = createCard(cardParams);
+
+    insertCard(placesList, cardElement, insertPosition);
+}
+
+function setProfileFormFields(editProfileForm, profileTitle, profileDescription) {
+    editProfileForm.elements['name'].value = profileTitle;
+    editProfileForm.elements['description'].value = profileDescription;
+}
+
+function handleProfileFormSubmit(evt, profileTitleElement, profileDescriptionElement) {
+    evt.preventDefault();
+
+    const form = evt.target;
+    const newTitle = form.name.value;
+    const newDescription = form.description.value;
+
+    profileTitleElement.textContent = newTitle;
+    profileDescriptionElement.textContent = newDescription;
+
+    closeModal(editProfileModal, modalClasses.isOpened);
+}
+
+function handleNewCardFormSubmit(evt) {
+    evt.preventDefault();
+
+    const form = evt.target;
+    const cardData = {name: form['place-name'].value, link: form['link'].value}
+
+    renderCard(cardData, INSERT_POSITION.START);
+    closeModal(newCardModal, modalClasses.isOpened);
+    form.reset();
+}
+
+function initializeCards(cards) {
     cards.forEach(cardData => {
-        const cardElement = createCard({cardTemplate, cardData, cardSelectors});
-        placesList.append(cardElement);
+        renderCard(cardData, INSERT_POSITION.END);
     });
 }
 
-renderCards(initialCards);
+initializeCards(initialCards);
+addButton.addEventListener('click', () => {
+    openModal(newCardModal, modalClasses.isOpened);
+});
+
+editProfileButton.addEventListener('click', () => {
+    setProfileFormFields(editProfileForm, profileTitle.textContent, profileDescription.textContent);
+    openModal(editProfileModal, modalClasses.isOpened);
+});
+
+editProfileForm.addEventListener('submit', (evt) => {
+    handleProfileFormSubmit(evt, profileTitle, profileDescription);
+});
+
+newCardForm.addEventListener('submit', handleNewCardFormSubmit);
+
+allModalWindows.forEach((modal) => {
+    modal.addEventListener('click', function (evt) {
+        const isClickOnCloseButton = evt.target.classList.contains(modalClasses.closeButton);
+        const isClickOnOverlay = evt.target === this;
+
+        if (isClickOnCloseButton || isClickOnOverlay) {
+            closeModal(modal, modalClasses.isOpened);
+        }
+    });
+});
